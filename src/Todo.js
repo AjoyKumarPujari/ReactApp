3bb88892af4e4a4d91a9113429635099b37b66d7
Ajoy Kumar Pujari <ajoykumarpujari@gmail.com>
2024-07-26 00:00:18 +0530

    Check rendering in profile

--- src/Todo.js
+++ src/Todo.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import TodoList from "./TodoList";
+import './Todo.css';
 let todos = [
     {id:1, data:'Todo 1'},
     {id:2, data:'Todo 2'},
@@ -8,6 +9,7 @@ let todos = [
 function Todo(){
     const [todos, setTodos] = useState([]);
     const [newTodo, setNewTodo] = useState(" ");
+    const [isFinished, setIsFinished] = useState(" ");
     return(
         <div className="todo">
              {/* <TodoList todos={todos}/>   */}
@@ -15,7 +17,9 @@ function Todo(){
             onChange={(event)=>  setNewTodo(event.target.value)}
             /><br/>
             <button onClick={() => setTodos ([...todos, newTodo])}>Add Todo</button>
-            <TodoList todos ={todos} />
+            <TodoList todos ={todos} /><br/>
+            <span className={(isFinished) ? 'todo-done' : 'todo-notdone'}>Todo Items</span><br/>
+            <button onClick={() => setIsFinished (!isFinished)}>{(isFinished)? 'Redo': 'Done'}</button>
         </div>
     )   
 }
